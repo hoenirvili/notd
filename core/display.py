cd03cc9d0a79d7d5dafdbb1df3ac9b37b6c09e0e
hoenirvili <hoenirvili@gmail.com>
2017-07-30 01:44:54 +0300

    Add debug info
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- core/display.py
+++ core/display.py
@@ -8,17 +8,16 @@ except:
 
 
 digits = {
-    ' ': (0,0,0,0,0,0,0),
-    '0': (1,1,1,1,1,1,0),
-    '1': (0,1,1,0,0,0,0),
-    '2': (1,1,0,1,1,0,1),
-    '3': (1,1,1,1,0,0,1),
-    '4': (0,1,1,0,0,1,1),
-    '5': (1,0,1,1,0,1,1),
-    '6': (1,0,1,1,1,1,1),
-    '7': (1,1,1,0,0,0,0),
-    '8': (1,1,1,1,1,1,1),
-    '9': (1,1,1,1,0,1,1)
+    0: (1,1,1,1,1,1,0),
+    1: (0,1,1,0,0,0,0),
+    2: (1,1,0,1,1,0,1),
+    3: (1,1,1,1,0,0,1),
+    4: (0,1,1,0,0,1,1),
+    5: (1,0,1,1,0,1,1),
+    6: (1,0,1,1,1,1,1),
+    7: (1,1,1,0,0,0,0),
+    8: (1,1,1,1,1,1,1),
+    9: (1,1,1,1,0,1,1)
 }
 
 time = 0.001
@@ -30,17 +29,31 @@ class Display:
 
         self.number = arguments['number']
         self.gpio = arguments['config']['gpio_conf']
-        self.transistor = arguments['config']['transistor_conf']
+        self.transistors = arguments['config']['transistor_conf']
         self.led = arguments['config']['led_conf']
         self.green = arguments['green']
         self.red = arguments['red']
 
     def run(self):
+        nums = list()
+        a = self.number
+        while a != 0:
+             nums.append(a%10)
+             a = int(a/10)
+
+        nums.reverse()
+        n = len(nums)
+
+        for num in nums:
+            for enum in digits[num]:
+
         self._setup_gpio()
-        pass
+
+    def clean(self):
+        self._cleanup_gpio()
 
     def _setup_gpio(self):
-        # Ppin numbers on the P1 header of the Raspberry Pi board. 
+        # Pin numbers on the P1 header of the Raspberry Pi board. 
         # The advantage of using this numbering system is that your 
         # hardware will always work, regardless of the board revision of the RPi.
         GPIO.setmode(GPIO.BOARD)
@@ -49,17 +62,21 @@ class Display:
         # if RPi.GPIO detects that a pin has been configured 
         # to something other than the default (input), you get a 
         # warning when you try to configure a script. 
-        GPIO.setwarnings(True)
+        GPIO.setwarnings(False)
+        self._all_pins_low()
+
+    def _all_pins_low(self):
         for gpio in self.gpio:
            GPIO.setup(gpio['pin'], GPIO.OUT, initial=GPIO.LOW)
-        
+
         for base in self.transistors:
             GPIO.setup(base['pin'], GPIO.OUT, initial=GPIO.LOW)
 
-        GPIO.setup(green['pin'], GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(red['pin'], GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.led[0]['pin'], GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.led[1]['pin'], GPIO.OUT, initial=GPIO.LOW)
 
-    def _cealup_gpio(self):
+
+    def _cleanup_gpio(self):
         GPIO.cleanup()
 
 
--- core/notd.py
+++ core/notd.py
@@ -7,13 +7,17 @@ from display import Display
 
 def main():
     options = Arguments(argv).parse()
-    
+
     if options == None:
         # daemon = Daemon()
         # daemon.start()
         return
 
-    Display(options).run()
+    display = Display(options)
+    print("[*] Running the display mode")
+    display.run()
+    print("[*] Exit the display mode, clean up")
+    display.clean()
 
 if __name__ == "__main__":
     main()
